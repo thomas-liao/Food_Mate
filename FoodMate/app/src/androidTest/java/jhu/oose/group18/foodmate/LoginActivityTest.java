359ea56b66c96dbe985564ca853dd8f0488f6c42
VinceS <vincetrsc@gmail.com>
2018-11-16 08:22:48 -0500

    first test for login

---
+++ FoodMate/app/src/androidTest/java/jhu/oose/group18/foodmate/LoginActivityTest.java
@@ -0,0 +1,44 @@
+package jhu.oose.group18.foodmate;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> loginActivityActivityTestRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+
+    private String username = "Amy";
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testUserLogin()
+    {
+        // input some text in the edit text
+//        Espresso.onView(withId(R.id.etTextToChange)).perform(typeText(mName));
+        // close soft keyboard
+//        Espresso.closeSoftKeyboard();
+        // perform button click
+        Espresso.onView(withId(R.id.btn_login)).perform(click());
+        // checking the text in the textview
+//        Espresso.onView(withId(R.id.tvChangedText)).check(matches(withText(mName)));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
