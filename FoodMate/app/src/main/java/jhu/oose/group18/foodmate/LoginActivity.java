f121587bb82225119c1eae33441ee9015769f246
MengXiangge <xianggemeng@gmail.com>
2018-11-16 17:07:59 -0500

    try to add GPS

--- FoodMate/app/src/main/java/jhu/oose/group18/foodmate/LoginActivity.java
+++ FoodMate/app/src/main/java/jhu/oose/group18/foodmate/LoginActivity.java
@@ -1,6 +1,10 @@
 package jhu.oose.group18.foodmate;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -50,6 +54,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                System.out.println(location.toString());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+// Register the listener with the Location Manager to receive location updates
+        try {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        } catch (SecurityException e) {
+            System.out.println(e);
+        }
 
         _loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,7 +132,7 @@ public class LoginActivity extends AppCompatActivity {
 
                 try {
                     RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-                    String URL = "http://10.0.2.2:8080/login";
+                    String URL = "https://food-mate.herokuapp.com/login";
 //                    String URL = "https://food-mate.herokuapp.com/login";
                     JSONObject jsonBody = new JSONObject();
                     jsonBody.put("userName", _usernameText.getText().toString());
--- FoodMate/app/src/main/java/jhu/oose/group18/foodmate/MyApplication.java
+++ FoodMate/app/src/main/java/jhu/oose/group18/foodmate/MyApplication.java
@@ -1,5 +1,10 @@
 package jhu.oose.group18.foodmate;
 import android.app.Application;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 
 public class MyApplication extends Application {
     String globalVariable="My Global Variable";
