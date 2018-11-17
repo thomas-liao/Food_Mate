29650c49def8b83ea1ae8a88912f2a9890cf8330
yukaliu07 <yliu@nurhopsi.org>
2018-11-16 21:53:51 -0500

    change recommendation view

--- FoodMate/app/src/main/java/jhu/oose/group18/foodmate/RecommendationActivity.java
+++ FoodMate/app/src/main/java/jhu/oose/group18/foodmate/RecommendationActivity.java
@@ -1,57 +1,143 @@
 package jhu.oose.group18.foodmate;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RecommendationActivity extends AppCompatActivity {
+    private RecyclerView mList;
+    JSONArray jsonArr;
 
-    String[] nameArray = {"recommendation","recommendation" };
+    private LinearLayoutManager linearLayoutManager;
+    private DividerItemDecoration dividerItemDecoration;
+    private List<Message> messageList;
+    private RecyclerView.Adapter adapter;
 
-    String[] infoArray = {
-            "this is a test message",
-            "this is a test message",
-            "this is a test message",
+    MyApplication application;
+    private String url;
 
-    };
 
-    Integer[] imageArray = {R.drawable.restaurant_logo,
-            R.drawable.restaurant_logo,
-            R.drawable.restaurant_logo,
-            R.drawable.restaurant_logo,
-            R.drawable.restaurant_logo,
-            R.drawable.restaurant_logo,
-            R.drawable.restaurant_logo};
+    private void getData() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
 
-    ListView listView;
+        application = (MyApplication) getApplication();
+        url = "https://food-mate.herokuapp.com/user/" + application.userId + "/host/restaurants";
+        System.out.println(url);
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // display response
+                        try {
+                            jsonArr = new JSONArray(response);
+                            for (int i = 0; i < jsonArr.length(); i++) {
+                                JSONObject jsonObj = jsonArr.getJSONObject(i);
+                                System.out.println(jsonObj);
+                                Message message = new Message();
+                                message.setName(jsonObj.getString("name"));
+                                message.setCategory(jsonObj.getString("category"));
+                                message.setPic(R.drawable.restaurant_logo);
+                                messageList.add(message);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            progressDialog.dismiss();
+                        }
+                        adapter.notifyDataSetChanged();
+                        progressDialog.dismiss();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley", error.toString());
+                progressDialog.dismiss();
+            }
+        });
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recommendation);
+        getRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 50000;
+            }
 
-        ListAdapter whatever = new ListAdapter(this, R.layout.recommendation_row, nameArray, infoArray, imageArray);
-        listView = (ListView) findViewById(R.id.recommendation_list);
-        listView.setAdapter(whatever);
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
 
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(ListRestaurantActivity.this, PostActivity.class);
-//                String message = nameArray[position];
-//                intent.putExtra("restaurantSelected", message);
-//                startActivity(intent);
-//            }
-//        });
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
 
+            }
+        });
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(getRequest);
     }
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendation);
+
+        mList = findViewById(R.id.recommendation_list);
+
+        messageList = new ArrayList<>();
+        adapter = new MyRecyclerViewAdapter(getApplicationContext(), messageList, new CustomItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Intent intent = new Intent(RecommendationActivity.this, PostActivity.class);
+                String message = messageList.get(position).getName();
+                MyApplication application = (MyApplication) getApplication();
+                try {
+                    application.restaurantId = jsonArr.getJSONObject(position).getInt("id");
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                intent.putExtra("restaurantSelected", message);
+                startActivity(intent);
+            }
+        });
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        dividerItemDecoration = new DividerItemDecoration(mList.getContext(), linearLayoutManager.getOrientation());
+
+        mList.setHasFixedSize(true);
+        mList.setLayoutManager(linearLayoutManager);
+        mList.addItemDecoration(dividerItemDecoration);
+        mList.setAdapter(adapter);
+
+        getData();
+    }
 }
+
